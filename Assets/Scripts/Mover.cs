6e9a242f317eaa6c160caafb3dbb66a2ee5b223b
regorDam <regorm88@gmail.com>
2015-06-07 19:40:12 +0200

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0.0f, 0.0f, 1) * speed;//transform.forward * speed;
+	}
+}
